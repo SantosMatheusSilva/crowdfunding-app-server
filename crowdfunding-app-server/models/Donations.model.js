bf6756e87a1ba05d62de983cc3d95337d980f80d
SantosMatheusSilva <mathsantos52@gmail.com>
2024-06-24 15:55:53 +0100

    donations.model fixed

--- crowdfunding-app-server/models/Donations.model.js
+++ crowdfunding-app-server/models/Donations.model.js
@@ -4,12 +4,15 @@ const Schema = mongoose.Schema;
 
 const DonationSchema = new Schema({
      amount: { type: Number, required: true },
+     currency: { type: String, default: "EUR" },
      date: { type: Date, default: Date.now },
-     donor: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-     paymentMethod: { type: String, required: true, enum: [ 'credit_card', 'paypal', 'other'] }, /* <<-- depends on the api we using to simulate payments*/
-     status: { type: String, enum: ['pending', 'completed', 'canceled']}, 
-     campaign: { type: mongoose.Schema.Types.ObjectId, ref: 'Campaign' },
+     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+     paymentMethod: { type: String, required: true, enum: [ 'card'] }, /* <<-- depends on the api we using to simulate payments*/
+     donationStatus: { type: String, enum: ['pending', 'completed', 'canceled']},
+     campaign: { type: mongoose.Schema.Types.ObjectId, ref: 'Campaign' } || { type: mongoose.Schema.Types.ObjectId, ref: 'Institution' },
      comments: {type: String, default: '', required: false },
+     paymentIntentId: {type: String, default: '', required: false },
+     clientSecret: {type: String, default: '', required: false },
     });
 
 
