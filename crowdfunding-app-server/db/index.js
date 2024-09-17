e162da27fa711a35522d314597c0b8398049bdf2
SantosMatheusSilva <mathsantos52@gmail.com>
2024-09-17 16:32:13 +0100

    mongo connection fixed

--- crowdfunding-app-server/db/index.js
+++ crowdfunding-app-server/db/index.js
@@ -1,5 +1,6 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require("mongoose");
 require('dotenv').config();
 
@@ -7,7 +8,7 @@ require('dotenv').config();
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI = process.env.MONGODB_URI;
-mongoose
+/* mongoose
   .connect(MONGO_URI)
   .then((x) => {
     const dbName = x.connections[0].name;
@@ -17,5 +18,25 @@ mongoose
     console.error("Error connecting to mongo: ", err);
   });
 
-  console.log('mongoDb URI', process.env.MONGODB_URI);
+  console.log('mongoDb URI', process.env.MONGODB_URI); */
   
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
